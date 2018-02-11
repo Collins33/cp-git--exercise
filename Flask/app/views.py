f0e08c1f49ba651be403069572ed56403f254486
Collins njau <collinsnjau39@gmail.com>
2018-02-11 16:55:43 +0300

    I have rendered the templates

--- Flask/app/views.py
+++ Flask/app/views.py
@@ -1,1 +1,8 @@
 #contains all the routes and the functions to render html
+
+from app import app
+from flask import render_template
+
+@app.route('/')
+def index():
+    return render_template("index.html")
--- Flask/run.py
+++ Flask/run.py
@@ -1,2 +1,13 @@
 #the app entry point
 # we run it to start the flask server
+#running it:
+# export FLASK_APP=run.py
+#flask run
+
+
+
+from app import app
+
+
+if __name__ == '__main__':
+    app.run()
