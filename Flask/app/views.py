8a3e97a1fc2dd0a6cb8c0dcdaedc724b58db3230
Collins njau <collinsnjau39@gmail.com>
2018-02-11 22:22:42 +0300

    I have added the links to other templates and the link to css

--- Flask/app/views.py
+++ Flask/app/views.py
@@ -1,8 +1,18 @@
 #contains all the routes and the functions to render html
 
-from app import app
-from flask import render_template
+from flask import Flask,render_template
+#create instance of flask class
+app=Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template("index.html")
+
+@app.route('/about')
+def about():
+    return render_template("about.html")  
+
+
+
+if __name__ == '__main__':
+  app.run(debug=True)
