c304306c7f99361d9df94b29c3a5aa5ff3aa828a
Collins njau <collinsnjau39@gmail.com>
2018-02-16 13:26:55 +0300

    I have added the addItem method and bound it in the class constructor

--- todolist/src/components/form.js
+++ todolist/src/components/form.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
 
 class Form extends Component{
+
+    constructor(props,context){
+        super(props,context)
+
+        this.addItem=this.addItem.bind(this)
+
+    }
+
+    addItem(e){
+        
+    }
     render(){
         return(
             <div className="row">
-            <form className="form-inline">
+            <form className="form-inline" onSubmit={this.addItem}>
 
             <div className="form-group">
             <input  className="form-control"/>
