7d7412b5813ff59fcf5d17bef44ba41746d618d6
Collins njau <collinsnjau39@gmail.com>
2018-02-16 13:42:07 +0300

    I have added the propperty on the input to allow us to access the input element

--- todolist/src/components/form.js
+++ todolist/src/components/form.js
@@ -5,12 +5,17 @@ class Form extends Component{
     constructor(props,context){
         super(props,context)
 
+        this.state={
+            //responsible for storing all our items when we enter them
+            items:[]
+        }
+
         this.addItem=this.addItem.bind(this)
 
     }
 
     addItem(e){
-        
+
     }
     render(){
         return(
@@ -18,7 +23,7 @@ class Form extends Component{
             <form className="form-inline" onSubmit={this.addItem}>
 
             <div className="form-group">
-            <input  className="form-control"/>
+            <input  className="form-control" placeholder="enter item" ref={(a) => this._inputElement=a}/>
             </div>
             <button className="btn btn-danger" type="submit">ADD ITEM</button>
 
